d4eb8d26d9882da4a2eecccfeb2405fc85d825f7
artorias961 <artorias961@gmail.com>
2024-02-07 18:56:08 -0800

    updated most projects

--- config/projects.ts
+++ config/projects.ts
@@ -32,10 +32,11 @@ export interface Project {
 }
 
 export const projects: Project[] = [
+  // Project 1 <-----------------------------------------------------
   {
     title: '3D Geometry Reconstruction of Medical Images',
-    slug: '3DGeometryReconstructionOfMedicalImages',
-    banner: '/static/projects/3DGeometryReconstructionOfMedicalImages/3DGeometryReconstructionOfMedicalImages.png',
+    slug: '3D-Geometry-Reconstruction-Of-Medical-Images',
+    banner: '/static/projects/3DGeometryReconstructionOfMedicalImages/medical_images.png',
     website: 'https://github.com/mbrieu/3D-Geometry-Recontruction-of-Medical-Images',
     description:
       'The project focused on using numerical tools to analyze medical images and construct a 3D model of the organs present in the image. The goal was to provide a detailed 3D model that could be easily manipulated for use in the medical field. The focus was on Magnetic Resonance Images (MRI) of the pelvic anatomy of women patients, with an emphasis on three main organs, the bladder, vagina, and rectum. To construct the 3D models from the MRI scans computer vision was utilized as the main tool for image analysis. An object detection tool was developed and trained to detect, differentiate, and label the bladder, vagina, and rectum. These same images were converted to a greyscale, which allowed the organs of interest to be isolated as well as identified by drawing contours around them. An ellipse was then fitted into each contour, and from these ellipses certain parameters could be obtained. Including the center point (x, y, z), the length (l), height (h) and orientation (n). This data can then be utilized to create a 3D CAD model of the patients’ pelvic organs.',
@@ -61,9 +62,11 @@ export const projects: Project[] = [
     },
     subProjects: [],
   },
+
+  // Project 2 <-----------------------------------------------------
   {
     title: 'Mechanical Keyboards Design',
-    slug: 'MechanicalKeyboard',
+    slug: 'Mechanical-Keyboard',
     banner: '/static/projects/mechanical_keyboard/mechanical_keyboard.jpg',
     website: 'https://github.com/artorias961/Mechanical-Keyboards',
     description: `Peapods is a new type of decentralized social network that values real human interaction. The platform helps you become closer with your friends and make new ones. We value kindness, friendship, user privacy, and genuine connection. Together we can cure boredom, conquer loneliness, and fulfill the original promise of the internet to connect us in ways that matter.`,
@@ -80,9 +83,11 @@ export const projects: Project[] = [
     },
     subProjects: [],
   },
+
+  // Project 3 <-----------------------------------------------------
   {
     title: 'Robotics Dog',
-    slug: 'RoboticsDog',
+    slug: 'Robotics-Dog',
     website: 'https://github.com/artorias961/robotics-dog',
     banner: '/static/projects/Robotic_Dog/robotic_dog.jpg',
     description:
@@ -114,19 +119,24 @@ export const projects: Project[] = [
     },
     subProjects: [],
   },
+
+  // Project 4 <-----------------------------------------------------
   {
-    title: 'Velvet Live',
-    slug: 'velvet-live',
+    title: 'IoT Edge Node of Home Assistant',
+    slug: 'IoT-Edge-Node-of-Home-Assistant',
     banner: '/static/projects/velvet-live/banner.png',
-    website: 'https://boom.london',
-    description: `Connect with shoppers who need your advice and expertise when they're shopping online. Assist and sell to customers the same way you would in-store, but now through chat and video straight from the app.`,
+    website: 'https://github.com/artorias961/nRF-Sensor-Django-Project',
+    description: `This project utilizes Nordic Semiconductor's nRF52DK and STM X-NUCLEO-IKS01A3 to create a sensor-based system. Employing ZephyrRTOS and Bluetooth Low Energy (BLE), the nRF52DK gathers data from sensors like LSM6DSO (MEMS 3D accelerometer + 3D gyroscope), LIS2MDL (MEMS 3D magnetometer), LIS2DW12 (MEMS 3D accelerometer), LPS22HH (MEMS pressure sensor), HTS221 (capacitive digital relative humidity and temperature), and STTS751 (temperature). The goal is to develop a simplified system akin to Raspberry Pi Home Assistant, leveraging the Django framework. Data transfer occurs via Bluetooth GATT, where the nRF52DK acts as a BLE server reading commands from Redis messages hosted on Docker. Responses are then relayed back to the Django server via Redis pub/sub channels. Utilizing Django Channels, WebSocket messages are transmitted to the Django site router and then to the internet for further processing.`,
     repository: null,
     stack: [
-      Stack.javascript,
+      Stack.ZephyrRTOS,
       Stack.c,
-      Stack.computervision,
+      Stack.docker,
       Stack.cplusplus,
-      Stack.CSS,
+      Stack.redis,
+      Stack.django,
+      Stack.python,
+      
     ],
     screenshots: [
       'https://firebasestorage.googleapis.com/v0/b/portfolio-8fa71.appspot.com/o/projects%2Fvelvet%2Fscreenshots%2Fscreenshot-1.png?alt=media&token=b9b044ce-a3af-402f-a1db-1ae61621bb18',
@@ -139,71 +149,13 @@ export const projects: Project[] = [
       'https://firebasestorage.googleapis.com/v0/b/portfolio-8fa71.appspot.com/o/projects%2Fvelvet%2Fscreenshots%2Fscreenshot-4.png?alt=media&token=3dcdc3bd-cbb7-4d8e-9368-306158cce40d',
     ],
     deployment: {
-      web: 'https://boom.london',
-      android: 'https://play.google.com/store/apps/details?id=app.boom.mobile',
+      web: 'https://github.com/artorias961/nRF-Sensor-Django-Project',
+      //android: 'https://play.google.com/store/apps/details?id=app.boom.mobile',
     },
     subProjects: [],
   },
-  {
-    title: 'Stewards',
-    slug: 'stewards',
-    website: 'https://stewards.app',
-    repository: null,
-    banner: '/static/projects/stewards/banner.png',
-    description:
-      'Mobile ordering solution with robot delivery. Stewards also provides variety of tools like staff application to manage orders, self checkout solution and admin dashboard.',
-    stack: [
-      Stack.javascript,
-      Stack.c,
-      Stack.cplusplus,
-      Stack.python,
-      Stack.git,
-    ],
-    screenshots: [
-      'https://firebasestorage.googleapis.com/v0/b/portfolio-8fa71.appspot.com/o/projects%2Fstewards%2Fscreenshots%2Fscreenshot-1.png?alt=media&token=ba9d7163-8df6-43ae-b804-01cd221cc593',
-      'https://firebasestorage.googleapis.com/v0/b/portfolio-8fa71.appspot.com/o/projects%2Fstewards%2Fscreenshots%2Fscreenshot-2.png?alt=media&token=c8194dc7-08cf-4b8c-a3ad-f6c643291a95',
-      'https://firebasestorage.googleapis.com/v0/b/portfolio-8fa71.appspot.com/o/projects%2Fstewards%2Fscreenshots%2Fscreenshot-3.png?alt=media&token=37c14308-767c-41c7-a064-df8b63cd18e8',
-      'https://firebasestorage.googleapis.com/v0/b/portfolio-8fa71.appspot.com/o/projects%2Fstewards%2Fscreenshots%2Fscreenshot-4.png?alt=media&token=6ad6b4f6-ba09-423d-bd3f-b06ae093113a',
-      'https://firebasestorage.googleapis.com/v0/b/portfolio-8fa71.appspot.com/o/projects%2Fstewards%2Fscreenshots%2Fscreenshot-5.png?alt=media&token=cd2945c0-af97-4b3d-a503-d52174232a8e',
-      'https://firebasestorage.googleapis.com/v0/b/portfolio-8fa71.appspot.com/o/projects%2Fstewards%2Fscreenshots%2Fscreenshot-6.png?alt=media&token=6fcaeab6-d539-4f39-9985-e6ee85e28f0a',
-      'https://firebasestorage.googleapis.com/v0/b/portfolio-8fa71.appspot.com/o/projects%2Fstewards%2Fscreenshots%2Fscreenshot-7.png?alt=media&token=3c1fcd3e-4d93-4275-8d87-94a9b9236a9d',
-      'https://firebasestorage.googleapis.com/v0/b/portfolio-8fa71.appspot.com/o/projects%2Fstewards%2Fscreenshots%2Fscreenshot-8.png?alt=media&token=9733914a-61a9-41ed-b715-3334ca425588',
-    ],
-    deployment: {
-      web: 'https://stewards.app',
-      android:
-        'https://play.google.com/store/apps/details?id=app.stewards.customer',
-      ios: 'https://apps.apple.com/in/app/stewards-order-to-your-desk/id1479412118',
-    },
-    subProjects: [
-      {
-        title: 'Staff Management',
-        repository: null,
-        description:
-          'Staff app for stewards restaurant staff, easily update menu, product availability and take live orders from customers.',
-        deployment: {
-          android:
-            'https://play.google.com/store/apps/details?id=app.stewards.staff&hl=en',
-        },
-      },
-      {
-        title: 'Admin Dashboard',
-        repository: null,
-        description:
-          'Staff administration app for stewards restaurant partners. This helps restaurant administrators to easily manage orders, receipts, tables, restaurant info etc.',
-        deployment: {
-          web: 'https://manage.stewards.app',
-        },
-      },
-      {
-        title: 'Self Checkout',
-        repository: null,
-        description:
-          "Stewards self checkout solution for customers who don't like waiting. Available on demand for iPad and tablets.",
-        deployment: {},
-      },
-    ],
-  },
+
+  // Project 5 <-----------------------------------------------------
   {
     title: 'Celebrify',
     slug: 'celebrify',
@@ -230,29 +182,95 @@ export const projects: Project[] = [
     },
     subProjects: [],
   },
-  {
-    title: 'KCards',
-    slug: 'kcards',
-    banner: '/static/projects/kcards/banner.jpeg',
-    description:
-      'KCards is your one-stop replacement for paper cards. KCards help you to easily share your cards with other users and get real-time analytics on the shared cards.',
-    repository: 'https://github.com/karanpratapsingh/KCards',
-    stack: [Stack.c, Stack.cplusplus, Stack.computervision, Stack.CSS],
-    dimensions: [450, 270],
-    screenshots: [
-      'https://firebasestorage.googleapis.com/v0/b/portfolio-8fa71.appspot.com/o/projects%2Fkcards%2Fscreenshots%2Fscreenshot-1.png?alt=media&token=5de462e1-7368-49fd-843f-abd4cccb3317',
-      'https://firebasestorage.googleapis.com/v0/b/portfolio-8fa71.appspot.com/o/projects%2Fkcards%2Fscreenshots%2Fscreenshot-2.png?alt=media&token=30061b0b-5bed-47d5-bf43-16c44cf076e0',
-      'https://firebasestorage.googleapis.com/v0/b/portfolio-8fa71.appspot.com/o/projects%2Fkcards%2Fscreenshots%2Fscreenshot-3.png?alt=media&token=350180a1-f559-45e7-b887-dafd2cf9eb78',
-      'https://firebasestorage.googleapis.com/v0/b/portfolio-8fa71.appspot.com/o/projects%2Fkcards%2Fscreenshots%2Fscreenshot-4.png?alt=media&token=574212b2-cf17-4e11-b8c0-a3a81473aeec',
-      'https://firebasestorage.googleapis.com/v0/b/portfolio-8fa71.appspot.com/o/projects%2Fkcards%2Fscreenshots%2Fscreenshot-5.png?alt=media&token=ecff0737-b3b5-45cc-8d85-97b684f9c4b7',
-      'https://firebasestorage.googleapis.com/v0/b/portfolio-8fa71.appspot.com/o/projects%2Fkcards%2Fscreenshots%2Fscreenshot-6.png?alt=media&token=b49f3f8e-6596-47ab-b3e6-872f5f67d7fe',
-    ],
-    deployment: {
-      web: 'https://kcards-server.herokuapp.com',
-      android: 'https://play.google.com/store/apps/details?id=com.kcards',
-      ios: 'https://itunes.apple.com/us/app/kcards/id1461733524',
-    },
-    website: 'https://kcards-server.herokuapp.com',
-    subProjects: [],
-  },
+
+  // Project Example <-----------------------------------------------------
+  // {
+  //   title: 'Stewards',
+  //   slug: 'stewards',
+  //   website: 'https://stewards.app',
+  //   repository: null,
+  //   banner: '/static/projects/stewards/banner.png',
+  //   description:
+  //     'Mobile ordering solution with robot delivery. Stewards also provides variety of tools like staff application to manage orders, self checkout solution and admin dashboard.',
+  //   stack: [
+  //     Stack.javascript,
+  //     Stack.c,
+  //     Stack.cplusplus,
+  //     Stack.python,
+  //     Stack.git,
+  //   ],
+  //   screenshots: [
+  //     'https://firebasestorage.googleapis.com/v0/b/portfolio-8fa71.appspot.com/o/projects%2Fstewards%2Fscreenshots%2Fscreenshot-1.png?alt=media&token=ba9d7163-8df6-43ae-b804-01cd221cc593',
+  //     'https://firebasestorage.googleapis.com/v0/b/portfolio-8fa71.appspot.com/o/projects%2Fstewards%2Fscreenshots%2Fscreenshot-2.png?alt=media&token=c8194dc7-08cf-4b8c-a3ad-f6c643291a95',
+  //     'https://firebasestorage.googleapis.com/v0/b/portfolio-8fa71.appspot.com/o/projects%2Fstewards%2Fscreenshots%2Fscreenshot-3.png?alt=media&token=37c14308-767c-41c7-a064-df8b63cd18e8',
+  //     'https://firebasestorage.googleapis.com/v0/b/portfolio-8fa71.appspot.com/o/projects%2Fstewards%2Fscreenshots%2Fscreenshot-4.png?alt=media&token=6ad6b4f6-ba09-423d-bd3f-b06ae093113a',
+  //     'https://firebasestorage.googleapis.com/v0/b/portfolio-8fa71.appspot.com/o/projects%2Fstewards%2Fscreenshots%2Fscreenshot-5.png?alt=media&token=cd2945c0-af97-4b3d-a503-d52174232a8e',
+  //     'https://firebasestorage.googleapis.com/v0/b/portfolio-8fa71.appspot.com/o/projects%2Fstewards%2Fscreenshots%2Fscreenshot-6.png?alt=media&token=6fcaeab6-d539-4f39-9985-e6ee85e28f0a',
+  //     'https://firebasestorage.googleapis.com/v0/b/portfolio-8fa71.appspot.com/o/projects%2Fstewards%2Fscreenshots%2Fscreenshot-7.png?alt=media&token=3c1fcd3e-4d93-4275-8d87-94a9b9236a9d',
+  //     'https://firebasestorage.googleapis.com/v0/b/portfolio-8fa71.appspot.com/o/projects%2Fstewards%2Fscreenshots%2Fscreenshot-8.png?alt=media&token=9733914a-61a9-41ed-b715-3334ca425588',
+  //   ],
+  //   deployment: {
+  //     web: 'https://stewards.app',
+  //     android:
+  //       'https://play.google.com/store/apps/details?id=app.stewards.customer',
+  //     ios: 'https://apps.apple.com/in/app/stewards-order-to-your-desk/id1479412118',
+  //   },
+  //   subProjects: [
+  //     {
+  //       title: 'Staff Management',
+  //       repository: null,
+  //       description:
+  //         'Staff app for stewards restaurant staff, easily update menu, product availability and take live orders from customers.',
+  //       deployment: {
+  //         android:
+  //           'https://play.google.com/store/apps/details?id=app.stewards.staff&hl=en',
+  //       },
+  //     },
+  //     {
+  //       title: 'Admin Dashboard',
+  //       repository: null,
+  //       description:
+  //         'Staff administration app for stewards restaurant partners. This helps restaurant administrators to easily manage orders, receipts, tables, restaurant info etc.',
+  //       deployment: {
+  //         web: 'https://manage.stewards.app',
+  //       },
+  //     },
+  //     {
+  //       title: 'Self Checkout',
+  //       repository: null,
+  //       description:
+  //         "Stewards self checkout solution for customers who don't like waiting. Available on demand for iPad and tablets.",
+  //       deployment: {},
+  //     },
+  //   ],
+  // },
+
+  
+
+  // Project Example <-----------------------------------------------------
+  // {
+  //   title: 'KCards',
+  //   slug: 'kcards',
+  //   banner: '/static/projects/kcards/banner.jpeg',
+  //   description:
+  //     'KCards is your one-stop replacement for paper cards. KCards help you to easily share your cards with other users and get real-time analytics on the shared cards.',
+  //   repository: 'https://github.com/karanpratapsingh/KCards',
+  //   stack: [Stack.c, Stack.cplusplus, Stack.computervision, Stack.CSS],
+  //   dimensions: [450, 270],
+  //   screenshots: [
+  //     'https://firebasestorage.googleapis.com/v0/b/portfolio-8fa71.appspot.com/o/projects%2Fkcards%2Fscreenshots%2Fscreenshot-1.png?alt=media&token=5de462e1-7368-49fd-843f-abd4cccb3317',
+  //     'https://firebasestorage.googleapis.com/v0/b/portfolio-8fa71.appspot.com/o/projects%2Fkcards%2Fscreenshots%2Fscreenshot-2.png?alt=media&token=30061b0b-5bed-47d5-bf43-16c44cf076e0',
+  //     'https://firebasestorage.googleapis.com/v0/b/portfolio-8fa71.appspot.com/o/projects%2Fkcards%2Fscreenshots%2Fscreenshot-3.png?alt=media&token=350180a1-f559-45e7-b887-dafd2cf9eb78',
+  //     'https://firebasestorage.googleapis.com/v0/b/portfolio-8fa71.appspot.com/o/projects%2Fkcards%2Fscreenshots%2Fscreenshot-4.png?alt=media&token=574212b2-cf17-4e11-b8c0-a3a81473aeec',
+  //     'https://firebasestorage.googleapis.com/v0/b/portfolio-8fa71.appspot.com/o/projects%2Fkcards%2Fscreenshots%2Fscreenshot-5.png?alt=media&token=ecff0737-b3b5-45cc-8d85-97b684f9c4b7',
+  //     'https://firebasestorage.googleapis.com/v0/b/portfolio-8fa71.appspot.com/o/projects%2Fkcards%2Fscreenshots%2Fscreenshot-6.png?alt=media&token=b49f3f8e-6596-47ab-b3e6-872f5f67d7fe',
+  //   ],
+  //   deployment: {
+  //     web: 'https://kcards-server.herokuapp.com',
+  //     android: 'https://play.google.com/store/apps/details?id=com.kcards',
+  //     ios: 'https://itunes.apple.com/us/app/kcards/id1461733524',
+  //   },
+  //   website: 'https://kcards-server.herokuapp.com',
+  //   subProjects: [],
+  // },
 ];
